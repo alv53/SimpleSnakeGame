06bcdf58b04896502ab4c230f498ef300e07a72d
Alvin <alvjou@gmail.com>
2014-07-08 21:25:15 -0500

    Snake moves, and responds to arrow keys

--- SimpleSnakeGame.py
+++ SimpleSnakeGame.py
@@ -21,11 +21,11 @@ running = True
 speed = 5
 
 # Direction of the snake
-# 1 = right
-# 2 = down
-# 3 = left
-# 4 = up
-d = 1 
+# 0 = right
+# 1 = down
+# 2 = left
+# 3 = up
+d = 0 
 
 # List to hold the snake
 SnakeX = [300, 290, 280, 270]
@@ -33,33 +33,63 @@ SnakeY = [300, 300, 300, 300]
 
 # a block represents a segment of the snake
 block = pygame.Surface((10, 10))
-block.fill((0, 200, 100))
-
+block.fill((0,200,100))
 while running:
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			running = False
- # User pressed down on a key
-	if event.type == pygame.KEYDOWN:
-		# Figure out if it was an arrow key. If so
-		# adjust speed.
-		if event.key == pygame.K_LEFT:
-			print "left"
-		elif event.key == pygame.K_RIGHT:
-			print "right"
-		elif event.key == pygame.K_UP:
-			print "up"
-		elif event.key == pygame.K_DOWN:
-			print "down"
+		if event.type == pygame.KEYDOWN:
+			# Figure out if it was an arrow key. If so
+			# adjust speed.
+			if event.key == pygame.K_LEFT:
+				print "left"
+				if d != 0:
+					d = 2
+			elif event.key == pygame.K_RIGHT:
+				print "right"
+				if d != 2:
+					d = 0
+			elif event.key == pygame.K_UP:
+				print "up"
+				if d != 1:
+					d = 3
+			elif event.key == pygame.K_DOWN:
+				print "down"
+				if d != 3:
+					d = 1
+
+	screen.fill((255,255,255))
 
 	# Update snake's position
+	x = len(SnakeX)-1
+	while x > 0:
+		SnakeX[x] = SnakeX[x-1]
+		SnakeY[x] = SnakeY[x-1]
+		x-=1
 
+	if d == 0: 		#right
+		SnakeX[0] = SnakeX[1] + 10
+		SnakeY[0] = SnakeY[1]
+	elif d == 1: 	#down
+		SnakeX[0] = SnakeX[1]
+		SnakeY[0] = SnakeY[1] + 10
+	elif d == 2: 	#left
+		SnakeX[0] = SnakeX[1] - 10
+		SnakeY[0] = SnakeY[1]
+	else: 			#up
+		SnakeX[0] = SnakeX[1]
+		SnakeY[0] = SnakeY[1] - 10
+	
 	# Drawing
+	print SnakeX
+	print SnakeY
+	print "\n"
+
 	for x in range(len(SnakeX)):
 		screen.blit(block, (SnakeX[x],SnakeY[x]))
 	# Update
 	pygame.display.flip()
 
 	# 60 fps
-	clock.tick(60)
-pygame.quit()
+	clock.tick(10)
+# pygame.quit()
