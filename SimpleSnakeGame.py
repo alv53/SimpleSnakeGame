ec1e02bcaf5cb479c4abd54bcfa1924d87592845
Alvin <alvjou@gmail.com>
2014-07-08 20:58:44 -0500

    Created the static snake

--- SimpleSnakeGame.py
+++ SimpleSnakeGame.py
@@ -1,16 +1,12 @@
 import pygame
 
-# Define some colors
-BLACK	= (   0,   0,   0)
-WHITE	= ( 255, 255, 255)
-GREEN	= (   0, 255,   0)
-RED	  = ( 255,   0,   0)
-
-# Config options
-(width, height) = (700, 500)
+# Config
+width = 800
+height = 600
 
 # Create the window
 screen = pygame.display.set_mode((width, height))
+screen.fill((255,255,255))
 pygame.display.set_caption('Alvin\'s Test Game')
 pygame.display.flip()
 
@@ -20,10 +16,25 @@ clock = pygame.time.Clock()
 
 # Main program loop
 running = True
-x = 300
-y = 200
-x_speed = 10
-y_speed = 0
+
+# Init speed of the snake, will increment after every number of food eaten
+speed = 5
+
+# Direction of the snake
+# 1 = right
+# 2 = down
+# 3 = left
+# 4 = up
+d = 1 
+
+# List to hold the snake
+SnakeX = [300, 290, 280, 270]
+SnakeY = [300, 300, 300, 300]
+
+# a block represents a segment of the snake
+block = pygame.Surface((10, 10))
+block.fill((0, 200, 100))
+
 while running:
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
@@ -33,30 +44,19 @@ while running:
 		# Figure out if it was an arrow key. If so
 		# adjust speed.
 		if event.key == pygame.K_LEFT:
-			if not x_speed > 0:
-				x_speed = -10
-				y_speed = 0
-		if event.key == pygame.K_RIGHT:
-			if not x_speed < 0:
-				x_speed = 10
-				y_speed = 0
-		if event.key == pygame.K_UP:
-			if not y_speed > 0:
-				y_speed = -10
-				x_speed = 0
-		if event.key == pygame.K_DOWN:
-			if not y_speed < 0:
-				y_speed = 10
-				x_speed = 0
-				
+			print "left"
+		elif event.key == pygame.K_RIGHT:
+			print "right"
+		elif event.key == pygame.K_UP:
+			print "up"
+		elif event.key == pygame.K_DOWN:
+			print "down"
 
+	# Update snake's position
 
 	# Drawing
-	screen.fill(WHITE)
-	x += x_speed
-	y += y_speed
-	pygame.draw.rect(screen, RED, [x, y, 10, 10])
-	
+	for x in range(len(SnakeX)):
+		screen.blit(block, (SnakeX[x],SnakeY[x]))
 	# Update
 	pygame.display.flip()
 
